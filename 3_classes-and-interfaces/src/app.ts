1a924320d372b7a1c0af0a1de4585c72465439d5
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-25 00:25:36 +0300

    protected prop

--- 3_classes-and-interfaces/src/app.ts
+++ 3_classes-and-interfaces/src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   // private readonly id: string;
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -36,6 +36,13 @@ class AccountingDepartment extends Department {
     super(id, 'Accounting');
   }
 
+  addEmployee(name: string) {
+    if (name === 'Max') {
+      return;
+    }
+    this.employees.push(name);
+  }
+
   addReport(text: string) {
     this.reports.push(text);
   }
@@ -52,7 +59,8 @@ console.log(it);
 
 const accounting = new AccountingDepartment('d1', []);
 accounting.addEmployee('Costa');
-accounting.addEmployee('Muller');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
 accounting.addReport('Something went wrong');
 accounting.printEmployeeInformation();
 console.log(accounting);
