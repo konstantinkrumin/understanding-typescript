1438427de838c8675f8388bdc269d4f80fec5149
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-24 22:27:32 +0300

    init first lessons folders

---
+++ 3_classes-and-interfaces/src/app.ts
@@ -0,0 +1,33 @@
+class Department {
+  // private readonly id: string;
+  // private name: string;
+  private employees: string[] = [];
+
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const accounting = new Department('d1', 'Accounting');
+accounting.describe();
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe();
