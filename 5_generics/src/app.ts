1ce6859cae588843a5ba80977e78d06ec370956f
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-27 23:53:14 +0300

    generic types with classes

--- 5_generics/src/app.ts
+++ 5_generics/src/app.ts
@@ -36,3 +36,48 @@ console.log(countAndDescribe('Hi there!'));
 console.log(countAndDescribe(['Sports', 'Cooking']));
 console.log(countAndDescribe([]));
 console.log(countAndDescribe(''));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return 'Value: ' + obj[key];
+}
+
+console.log(extractAndConvert({ name: 'Costa' }, 'name'));
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(2);
+numberStorage.addItem(7);
+numberStorage.addItem(8);
+numberStorage.removeItem(2);
+console.log(numberStorage.getItems());
+
+// const objStorage = new DataStorage<object>();
+// const maxObj = { name: 'Max' };
+// objStorage.addItem({ name: 'Costa' });
+// objStorage.addItem(maxObj);
+// objStorage.removeItem(maxObj);
+// console.log(objStorage.getItems());
