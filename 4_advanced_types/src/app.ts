aab17b3ad973268259f7c5d7cca8da20ac5821c0
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-27 20:26:26 +0300

    function overload

--- 4_advanced_types/src/app.ts
+++ 4_advanced_types/src/app.ts
@@ -132,3 +132,20 @@ const errorBag: ErrorContainer = {
   email: 'Not a valid email!',
   username: 'Must start with a capital character!',
 };
+
+// FUNCTION OVERLOADS
+function add1(a: number, b: number): number;
+function add1(a: string, b: string): string;
+function add1(a: string, b: number): string;
+function add1(a: number, b: string): string;
+function add1(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+const result = add1('Costa', ' Coder');
+result.split(' ');
+
+const result1 = add1(1, 3);
