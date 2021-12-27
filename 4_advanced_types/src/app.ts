84e9755c2467010cf6a983ac228fbd313e689321
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-27 20:05:51 +0300

    type casting

--- 4_advanced_types/src/app.ts
+++ 4_advanced_types/src/app.ts
@@ -115,3 +115,9 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: 'bird', flyingSpeed: 10 });
+
+// TYPE CASTING
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
+const userInputElement = document.getElementById('user-input')! as HTMLInputElement;
+
+userInputElement.value = 'Hi there!';
