41be47d6506dc7f7011d80f9dd04c1793318c7e1
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-28 01:54:00 +0300

    added property decorator

--- 6_decorators/src/app.ts
+++ 6_decorators/src/app.ts
@@ -1,11 +1,27 @@
 function Logger(logString: string) {
+  console.log('LOGGER FACTORY');
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
   };
 }
 
+function WithTemplate(template: string, hookId: string) {
+  console.log('TEMPLATE FACTORY');
+  return function (constructor: any) {
+    console.log('Rendering template');
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = p.name;
+    }
+  };
+}
+
 @Logger('LOGGING - PERSON')
+@WithTemplate('<h1>My Person Objec</h1>', 'app')
 class Person {
   name = 'Max';
 
@@ -16,3 +32,32 @@ class Person {
 
 const pers = new Person();
 console.log(pers);
+
+// PROPERTY DECORATORS
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property decorator');
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(value: number) {
+    if (value > 0) {
+      this._price = value;
+    } else {
+      throw new Error('Invalid price - should be positive!');
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
