96cf690b65217e0002ee06ab57174dfcc327ae8b
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-27 21:20:41 +0300

    another generic function

--- 5_generics/src/app.ts
+++ 5_generics/src/app.ts
@@ -17,3 +17,22 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergeObj = merge({ name: 'Costa' }, { age: 27 });
 console.log(mergeObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value';
+  if (element.length === 0) {
+    descriptionText = 'Got 1 element';
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements';
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there!'));
+console.log(countAndDescribe(['Sports', 'Cooking']));
+console.log(countAndDescribe([]));
+console.log(countAndDescribe(''));
