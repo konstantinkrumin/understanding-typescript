4b2fc02dec2c1eb891b92ea3ae58989ff6e5e8f2
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-30 01:22:47 +0300

    reorganized as namespaces

---
+++ 8_modules-and-namespaces/src/decorators/autobind.ts
@@ -0,0 +1,14 @@
+namespace App {
+  // AUTOBIND DECORATOR
+  export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjDescriptor;
+  }
+}
