65aabcaa956b532c3092a45212fdfacc3f78ae22
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-30 16:30:22 +0300

    set up validators and transformers

--- 10_3rd-party-libraries-and-typescript/src/app.ts
+++ 10_3rd-party-libraries-and-typescript/src/app.ts
@@ -1,1 +1,29 @@
-// Code goes here!
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+
+import { Product } from './product.model';
+
+const products = [
+  { title: 'A Carpet', price: 29.99 },
+  { title: 'A Book', price: 10.99 },
+];
+
+// const newProd = new Product('', -5);
+// validate(newProd).then((errors) => {
+//   if (errors.length > 0) {
+//     console.log('VALIDATION ERRORS!');
+//     console.log(errors);
+//   } else {
+//     console.log(newProd.getInformation());
+//   }
+// });
+
+const loadedProducts = plainToClass(Product, products);
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
