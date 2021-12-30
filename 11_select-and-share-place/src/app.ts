1adb2f9bc6d7f9952d3b6a3bbffd55ce39276ca8
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-30 22:01:53 +0300

    set up google maps proj:

--- 11_select-and-share-place/src/app.ts
+++ 11_select-and-share-place/src/app.ts
@@ -1,11 +1,40 @@
+import axios from 'axios';
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'TEST';
+
+declare var google: any;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
-  // send this to Google's API!
+  axios
+    .get<GoogleGeocodingResponse>(`https://test.com/address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+    .then((response) => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch location');
+      }
+      const coordinates = response.data.results[0].geometry.location;
+
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: coordinates,
+        zoom: 8,
+      });
+
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form.addEventListener('submit', searchAddressHandler);
