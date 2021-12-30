d168417f48afdddeba524f00ad7b1bc5d4625745
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-30 22:47:19 +0300

    setting up components

--- 12_react-and-typescript/src/App.tsx
+++ 12_react-and-typescript/src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-function App() {
-  return <div className="App"></div>;
-}
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+
+const App: React.FC = () => {
+  const todos = [{ id: 't1', text: 'Finish the course' }];
+  return (
+    <div className="App">
+      <NewTodo />
+      <TodoList items={todos} />
+    </div>
+  );
+};
 
 export default App;
